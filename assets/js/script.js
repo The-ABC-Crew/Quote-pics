8e4bc9bffa1b97dfa00bd95e2b743efc38cf5855
CFM <cpierrelouis37@gmail.com>
2021-11-10 19:28:59 -0600

    Javascript update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,12 @@ var backgroundBtn = document.querySelector("#backgroundBtn");
 var picQuoteBtn = document.querySelector('#picQuoteButton');
 var picContainerEl = document.querySelector('.picContainer')
 
+// A class will randomly be assigned to the quote text
+var quoteClass = ['bottom-left', 'top-left', 'top-right', 'bottom-right', 'centered']
 
+//Words that won't be used to search for an image
 var exclusionArray = ['and', 'the', 'i', 'you', 'we', 'they', 'im',
-'them', 'at', 'is', 'not', 'of', 'to', 'from', 'for', 'who', 'what', 'how', 'why', 'where', 'when', 'it', 'its',
+'so','them', 'at', 'is', 'not', 'of', 'to', 'from', 'for', 'who', 'what', 'how', 'why', 'where', 'when', 'it', 'its',
 'he', 'she', 'a', 'youre', 'your', 'too', 'there', 'their', 'theyre', 'with', 'in', 'do', 'done', 'did',
 'our', 'are', 'about', 'that', 'thats', 'who', 'whose', 'mine', 'yours', 'by', 'yes', 'no', 'shit', 'bitch',
 'fuck', 'ass', 'whore', 'slut', 'asshole', 'bastard', 'god', 'goddamn', 'goddammit', 'penis', 'vagina', 'pussy',
@@ -16,14 +19,29 @@ var exclusionArray = ['and', 'the', 'i', 'you', 'we', 'they', 'im',
 ]
 
 
+var excludeSource = ['hitler', 'goebbels', 'stalin', 'bolsonaro']
+
+// API keys
+var quoteApiArr = ['7c60dc7b45msh2b451cd8e9e818cp154685jsn02718fac91a5', 'bf1a2d4e80msh94bbb945c71633dp1a715cjsnf031a1e266b3',
+'0238fd9419msh1a74ae4a30af76fp15d5bejsnaee83c201b23']
+var quotei = 2;
+
+var pexelsApiArr = ['563492ad6f9170000100000176391aec0ad4405c9c2d91c6edd1cebb', '563492ad6f91700001000001687d7a81df76457e89cf51ad832280ac']
+var pexelsi = 1;
+
 // Fetches data from QuotePark
 function fetchQuote() {
 
-    fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
+    if(quotei === 2){
+        quotei = 0;
+    } else {
+        quotei++;
+    }
+  fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "quotes15.p.rapidapi.com",
-            "x-rapidapi-key": "7c60dc7b45msh2b451cd8e9e818cp154685jsn02718fac91a5"
+            "x-rapidapi-key": quoteApiArr[quotei]
         }
     })
         .then(quoteResp => {
@@ -31,7 +49,11 @@ function fetchQuote() {
         })
         .then(quoteData => {
             var wordCount = quoteData.content.split(' ');
-            if (wordCount.length > 30) {
+            var quoteSrc = quoteData.originator.name.toLowerCase().split(' ');
+
+            //Checks to see if the quote is too long or if it originates from someone with... poor character
+            if (wordCount.length > 30 || excludeSource.includes(quoteSrc[1])) {
+
                 fetchQuote();
             } else {
                 displayQuotePic(quoteData);
@@ -41,14 +63,20 @@ function fetchQuote() {
 }
 
 // Fetches data from Pexels
-function fetchPic(term) {
+function fetchPic(quoteData) {
+    if(pexelsi === 1) {
+        pexelsi = 0;
+    } else {
+        pexelsi++;
+    }
+
 
 
     // Checks to see if you're searching for pic or just want a random pic
-    if (term === undefined) {
+    if (quoteData === undefined) {
         fetch("https://api.pexels.com/v1/curated?per_page=15", {
             headers: {
-                authorization: "563492ad6f91700001000001687d7a81df76457e89cf51ad832280ac"
+                authorization: pexelsApiArr[pexelsi]
             }
         }).then(picResp => {
             return picResp.json();
@@ -59,9 +87,10 @@ function fetchPic(term) {
 
 
     } else {
-        fetch("https://api.pexels.com/v1/search?query=" + term, {
+        var word = chooseRandomWord(quoteData.content);
+        fetch("https://api.pexels.com/v1/search?query=" + word, {
             headers: {
-                authorization: "563492ad6f91700001000001687d7a81df76457e89cf51ad832280ac"
+                authorization: pexelsApiArr[pexelsi]
             }
         })
             .then(picResp => {
@@ -70,7 +99,7 @@ function fetchPic(term) {
             })
             .then(picData => {
                 if (picData.total_results === 0) {
-                    fetchPic(term)
+                    fetchPic(quoteData)
                 } else {
                     var picLink = picData.photos[getRandomInt(picData.photos.length)].src.original;
 
@@ -98,17 +127,13 @@ function getRandomInt(max) {
 
 // Displays quote pic
 function displayQuotePic(quoteData) {
-    console.log(quoteData);
-    console.log(quoteData.content + ' - ' + quoteData.originator.name);
-
-    var word = chooseRandomWord(quoteData.content);
-
-    fetchPic(word);
+    
+    fetchPic(quoteData);
 
     if (picContainerEl.children.length > 1) document.querySelector('#quote').remove();
 
     var quoteEl = document.createElement('p');
-    quoteEl.setAttribute('class', 'centered')
+    quoteEl.setAttribute('class', quoteClass[getRandomInt(5)])
     quoteEl.setAttribute('id', 'quote')
     quoteEl.textContent = quoteData.content + ' - ' + quoteData.originator.name;
     picContainerEl.append(quoteEl);
@@ -118,7 +143,7 @@ function displayQuotePic(quoteData) {
 function chooseRandomWord(quote) {
     var words = quote.split(' ');
     var randomWord = words[getRandomInt(words.length)].toLowerCase();
-    randomWord = randomWord.replace(/[!,.?':"]/g,"");
+    randomWord = randomWord.replace(/[!,....'?':"]/g,"");
     for(var i = 0 ; i < exclusionArray.length ; i++){
         if(randomWord === exclusionArray[i]) randomWord = chooseRandomWord(quote);
     }
